1daa4f473fbfce88ef38cc7df0a87c16f5987173
apanayotov <asen.panayotov@gmail.com>
2014-07-20 20:34:22 +0300

    generating quadric pdf html

--- quadric.rb
+++ quadric.rb
@@ -1,16 +1,30 @@
+require "pdfkit"
+
 def generate_quadratic 
 	pool = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
 	return "#{pool.sample}x^2 + #{pool.sample}x + #{pool.sample}"
 end
 
+def make_html_and_pdf equations
+	myfile = File.new("equations.html", "w+") 
 
-def make_pdf 
+	myfile.puts "<html>"
+	myfile.puts "<title>Equations</title>"
+	myfile.puts "<style>body > div{clear:both;float:left; width:500px;}</style>"
+	myfile.puts "<body style=\"padding: 15px;\">"
+	
+	equations.each_with_index do |e,index|
+		myfile.puts "#{e}"
+		myfile.puts "<br><br>"	
+	end 
 
-end
+	myfile.puts "</body>"
+	myfile.puts "</html>"
 
-def make_html
-
+	kit = PDFKit.new(myfile)
+	kit.to_file("equations.pdf")
+	
 end
 
 
@@ -26,3 +40,4 @@ user_input.times do
 	equations << generate_quadratic
 end
 
+make_html_and_pdf equations
