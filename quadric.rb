c4d5a87075a78e12699d38af5741ed326afd4131
apanayotov <asen.panayotov@gmail.com>
2014-07-21 23:00:38 +0300

    dada

--- quadric.rb
+++ quadric.rb
@@ -1,36 +1,47 @@
 require "pdfkit"
 
-def generate_quadratic 
-	pool = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+pool = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 
+def generate_quadratic pool
 	return "#{pool.sample}x^2 + #{pool.sample}x + #{pool.sample}"
 end
 
-def generate_cubic
-	pool = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-
+def generate_cubic pool 
 	return "#{pool.sample}x^3 + #{pool.sample}x^2 + #{pool.sample}x + #{pool.sample}"
 end
 
-def make_html_and_pdf equations
-	myfile = File.new("equations.html", "w+") 
+def solving_quadratics equations
+	temp = equations.split
+	a = temp[0][0].to_i
+	b = temp[2][0].to_i
+	c = temp[4].to_i
+	d = (b*b)-(4*a*c)
+end
+
+def make_html_and_pdf equations, n
+	filename="test_#{n}"
+	myfile = File.new(filename+".html", "w+") 
 
 	myfile.puts "<html>"
 	myfile.puts "<title>Equations</title>"
 	myfile.puts "<style>body > div{clear:both;float:left; width:500px;}</style>"
 	myfile.puts "<body style=\"padding: 15px;\">"
+	myfile.puts "<h1>Test Number #{n}</h1>"
 	
 	equations.each_with_index do |e,index|
-		myfile.puts "#{e}"
-		myfile.puts "<br><br>"	
-	end 
+		myfile.puts "#{index}) "
+myfile.puts "#{e}"
+myfile.puts "<br><br>"	
+end 
+#p equations.class
+#p equations
 
-	myfile.puts "</body>"
-	myfile.puts "</html>"
+myfile.puts "</body>"
+myfile.puts "</html>"
 
-	kit = PDFKit.new(myfile)
-	kit.to_file("equations.pdf")
-	
+kit = PDFKit.new(myfile)
+kit.to_file(filename+".pdf")
+
 end
 
 
@@ -42,9 +53,15 @@ user_input =  user_input.to_i
 
 equations = Array.new
 
-user_input.times do 
-	equations << generate_quadratic
-	equations << generate_cubic
-end
+user_input.times do |n|
+	equations = [] 
+	10.times do 
+		quadric = (generate_quadratic pool)
+		cubic = (generate_cubic pool)
+		equations << quadric
+		solving_quadratics quadric
+		equations << cubic
+	end
 
-make_html_and_pdf equations
+	make_html_and_pdf equations, n
+end
