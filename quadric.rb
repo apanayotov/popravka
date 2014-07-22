3493f48f83807895369a5c76ce80d1ab4578f103
apanayotov <asen.panayotov@gmail.com>
2014-07-22 23:22:15 +0300

    final final let me pass

--- quadric.rb
+++ quadric.rb
@@ -16,7 +16,43 @@ def solving_quadratics equations
 	b = temp[2][0].to_i
 	c = temp[4].to_i
 	d = (b*b)-(4*a*c)
+	if  d > 0
+		x1 = (-b+Math.sqrt(d))/(2*a)
+		x2 = (-b-Math.sqrt(d))/(2*a)
+		return x1,x2
+	else
+		return "NRK"
+	end
+end
+
+def make_html_and_pdf_answers answers, n 
+	filename="test_answers_#{n}"
+	myfile = File.new(filename+".html", "w+") 
+
+	myfile.puts "<html>"
+	myfile.puts "<title>Equations</title>"
+	myfile.puts "<style>body > div{clear:both;float:left; width:500px;}</style>"
+	myfile.puts "<body style=\"padding: 15px;\">"
+	myfile.puts "<h1>Test Number #{n}</h1>"
 	
+	answers.each_with_index do |e,index|
+		myfile.puts "#{index}) "
+	e.each do |s|
+		if (s != "......") && (s != "NRK")
+			myfile.puts "x1= #{s[0]} x2=#{s[1]} "
+		else
+			myfile.puts"#{s}"
+		end
+
+	end
+
+myfile.puts "<br><br>"	
+myfile.puts "</body>"
+myfile.puts "</html>"
+end
+kit = PDFKit.new(myfile)
+kit.to_file(filename+".pdf")
+
 end
 
 def make_html_and_pdf equations, n
@@ -53,16 +89,20 @@ user_input = gets.chomp
 user_input =  user_input.to_i
 
 equations = Array.new
+answers = Array.new
 
 user_input.times do |n|
 	equations = [] 
+	answers = []
 	10.times do 
 		quadric = (generate_quadratic pool)
 		cubic = (generate_cubic pool)
-		equations << quadric
-		solving_quadratics quadric
-		equations << cubic
+		equations << quadric+" = 0"
+		answers << [(solving_quadratics quadric)]
+		equations << cubic +" = 0"
+		answers << [("......")]
 	end
-
+p answers	
+	make_html_and_pdf_answers((answers),n)	
 	make_html_and_pdf equations, n
 end
